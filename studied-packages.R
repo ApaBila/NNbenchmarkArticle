3360b5efb0d7fec2936284674eb542b7d66a746d
Christophe Dutang <dutangc@gmail.com>
2023-12-19 10:38:28 +0100

    updated list

--- studied-packages.R
+++ studied-packages.R
@@ -1,24 +1,38 @@
 library(RWsearch)
 crandb_down()
 
-fullNNlist <- setdiff(s_crandb("neural", "networks", mode="and"), "NNbenchmark")
+fullNNlist <- setdiff(s_crandb("neural", "network", mode="and"), "NNbenchmark")
 fullNNdep <- lapply(p_deps(fullNNlist, recursive = TRUE), sort)
 
-#fullNNdep[["deep"]]
+fullNNdep[["yager"]]
 
-primaryNNlist <- c("ANN2", "automl", "brnn", "CaDENCE", "deep", "deepNN", "elmNNRcpp", "h2o",
-                "keras", "neuralnet", "nnet", "nnlib2Rcpp", "RSNNS", "simpleNeural", 
+primaryNNlist <- c("ANN2", "automl", "Buddle", "brnn", "CaDENCE", "deep", "deepdive",
+                   "deepnet", "deepNN","ELMR", "elmNNRcpp", "h2o",
+                "keras", "monmlp",
+                "neuralnet", "nnet", "nnlib2Rcpp", "RSNNS", "simpleMLP", "simpleNeural", 
                 "tensorflow", "torch", "validann")
 secondaryNNlist <- c("aifeducation", "cito", "DamiaNN", "fastai", "innsight", "janus",
-                     "MachineShop", "NeuralNetTools", "NeuralSens", "tfestimators")
-toospecific <- c("appnn", "ConvertPar", "digitalDLSorteR", "DNMF", "edl", "evclass", 
-                 "ganDataModel", "GRNNs", "ImNN", "isingLenzMC", "leabRa", "learNN",
-                 "LilRhino", "quarrint", "rasclass", "reservr", "RTextTools", "rTG",
+                     "MachineShop", "NeuralNetTools", "NeuralSens", "nndiagram", "tfestimators")
+toospecific <- c("appnn", "cld2", "cld3", "ConvertPar", "cpfa", "deeptrafo", "deepregression", 
+                 "digitalDLSorteR", "DNMF", "DMLLZU", "edl", "EMDANNhybrid", "epicasting", 
+                 "evclust", "evclass", "evreg", "FWRGB",
+                 "ganDataModel", "GMDH2", "GRNNs", "grnn", "image.libfacedetection",
+                 "ImNN", "isingLenzMC", "leabRa", "learNN",
+                 "LilRhino", "lilikoi", "MARSANNhybrid", "MBMethPred", "ML2Pvae", "mrbin",
+                 "onnx", "passt", "qrnn",
+                 "quarrint", "rasclass", "reservr", "Rstg", "RTextTools", "rTG",
                  "scapGNN", "shattering", "SignacX", "sjSDM", "snap", "Sojourn.Data",
-                 "soundClass", "SpatialDDLS", "sr", "survivalmodels", "TeachNet",
-                 "TrafficBDE")
-timeserieNN <- c("hybridts", "long2lstmarray", "NlinTS", "nnfor", "sits", "tsensembler", "WaveletLSTM")
-moregeneralModelinglist <- c("BayesFluxR", "caret", "FuncNN", "gamlss.add", "GMDHreg", 
-                             "gnn", "ModTools", "neuralGAM", "predictoR", "ProcData", 
+                 "soundClass", "SpatialDDLS", "sr", "survivalmodels", "TeachNet", "trackdem",
+                 "TrafficBDE", "transformer", "vmdTDNN", "WaveletML", "yager")
+timeserieNN <- c("AriGaMyANNSVR", "ARIMAANN", "CEEMDANML", "ECTTDNN", "eemdTDNN",
+                 "GMDH", "hybridts", "LDNN", "long2lstmarray", "NlinTS", "nnfor", 
+                 "OptiSembleForecasting", "rnn", "sits", "stlTDNN", "TSANN", 
+                 "tsensembler", "tsfgrnn", "TSLSTM", "TSLSTMplus", "WaveletLSTM")
+multiModelist <- c("BayesFluxR", "caret", "EnsembleBase", "FuncNN", "gamlss.add", 
+                   "glmnetr", "GMDHreg", "gnn", "ModTools", "neuralGAM", 
+                   "predictoR", "ProcData", 
                              "radiant.model", "regressoR", "RGAN", "rMIDAS", "rminer", 
                              "spnn", "studyStrap", "traineR")
+
+
+setdiff(fullNNlist, c(primaryNNlist, secondaryNNlist, toospecific, timeserieNN, multiModelist))
