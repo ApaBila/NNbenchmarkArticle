5356346045ae7fc03cc07991ae152f3d21ac4da3
Christophe Dutang <dutangc@gmail.com>
2023-12-19 10:55:10 +0100

    Update studied-packages.R

--- studied-packages.R
+++ studied-packages.R
@@ -4,7 +4,7 @@ crandb_down()
 fullNNlist <- setdiff(s_crandb("neural", "network", mode="and"), "NNbenchmark")
 fullNNdep <- lapply(p_deps(fullNNlist, recursive = TRUE), sort)
 
-fullNNdep[["yager"]]
+fullNNdep[["gnn"]]
 
 primaryNNlist <- c("ANN2", "automl", "Buddle", "brnn", "CaDENCE", "deep", "deepdive",
                    "deepnet", "deepNN","ELMR", "elmNNRcpp", "h2o",
@@ -13,26 +13,27 @@ primaryNNlist <- c("ANN2", "automl", "Buddle", "brnn", "CaDENCE", "deep", "deepd
                 "tensorflow", "torch", "validann")
 secondaryNNlist <- c("aifeducation", "cito", "DamiaNN", "fastai", "innsight", "janus",
                      "MachineShop", "NeuralNetTools", "NeuralSens", "nndiagram", "tfestimators")
-toospecific <- c("appnn", "cld2", "cld3", "ConvertPar", "cpfa", "deeptrafo", "deepregression", 
+toospecific <- c("appnn", "BayesFluxR",
+                 "cld2", "cld3", "ConvertPar", "cpfa", "deeptrafo", "deepregression", 
                  "digitalDLSorteR", "DNMF", "DMLLZU", "edl", "EMDANNhybrid", "epicasting", 
-                 "evclust", "evclass", "evreg", "FWRGB",
-                 "ganDataModel", "GMDH2", "GRNNs", "grnn", "image.libfacedetection",
+                 "evclust", "evclass", "evreg", "FWRGB", "FuncNN", 
+                 "ganDataModel", "glmnetr", "GMDHreg", "GMDH2", "gnn", 
+                 "GRNNs", "grnn", "image.libfacedetection",
                  "ImNN", "isingLenzMC", "leabRa", "learNN",
                  "LilRhino", "lilikoi", "MARSANNhybrid", "MBMethPred", "ML2Pvae", "mrbin",
-                 "onnx", "passt", "qrnn",
-                 "quarrint", "rasclass", "reservr", "Rstg", "RTextTools", "rTG",
+                 "neuralGAM", "onnx", "passt", "predictoR", "ProcData", "qrnn",
+                 "quarrint", "rasclass", "regressoR", "reservr","RGAN",  "rMIDAS",
+                 "Rstg", "RTextTools", "rTG",
                  "scapGNN", "shattering", "SignacX", "sjSDM", "snap", "Sojourn.Data",
-                 "soundClass", "SpatialDDLS", "sr", "survivalmodels", "TeachNet", "trackdem",
+                 "soundClass", "SpatialDDLS", "spnn", "sr", "studyStrap", 
+                 "survivalmodels", "TeachNet", "trackdem",
                  "TrafficBDE", "transformer", "vmdTDNN", "WaveletML", "yager")
 timeserieNN <- c("AriGaMyANNSVR", "ARIMAANN", "CEEMDANML", "ECTTDNN", "eemdTDNN",
                  "GMDH", "hybridts", "LDNN", "long2lstmarray", "NlinTS", "nnfor", 
                  "OptiSembleForecasting", "rnn", "sits", "stlTDNN", "TSANN", 
                  "tsensembler", "tsfgrnn", "TSLSTM", "TSLSTMplus", "WaveletLSTM")
-multiModelist <- c("BayesFluxR", "caret", "EnsembleBase", "FuncNN", "gamlss.add", 
-                   "glmnetr", "GMDHreg", "gnn", "ModTools", "neuralGAM", 
-                   "predictoR", "ProcData", 
-                             "radiant.model", "regressoR", "RGAN", "rMIDAS", "rminer", 
-                             "spnn", "studyStrap", "traineR")
+multiModelist <- c("caret", "EnsembleBase", "gamlss.add", "ModTools", 
+                             "radiant.model",  "rminer",  "traineR")
 
 
 setdiff(fullNNlist, c(primaryNNlist, secondaryNNlist, toospecific, timeserieNN, multiModelist))
